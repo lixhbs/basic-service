aab45a274e142455cd7d19bca54fc7871239e4db
l.c.y <lixhbs@163.com>
2022-02-24 16:32:12 +0800

    Initial commit

--- src/main/java/com/basic/admin/AdminServiceApplication.java
+++ src/main/java/com/basic/admin/AdminServiceApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@MapperScan("com.basic.admin")
 public class AdminServiceApplication
 {
 	public static void main(String[] args) {
