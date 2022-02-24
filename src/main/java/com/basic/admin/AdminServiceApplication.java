cc705774bafd2d253ef35dabfca4bf9f7ca59dee
l.c.y <lixhbs@163.com>
2022-02-24 16:34:15 +0800

    修改名称

--- src/main/java/com/basic/admin/AdminServiceApplication.java
+++ src/main/java/com/basic/admin/AdminServiceApplication.java
@@ -1,14 +1,7 @@
 package com.basic.admin;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AdminServiceApplication
